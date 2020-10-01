80e908a45b0e827f52e020de41b3f6f1e8709092
Yash Purohit <45822567+Y1a2s3h4@users.noreply.github.com>
2020-10-01 13:11:13 +0530

    Added code for timeout 0

--- puppeteer.js
+++ puppeteer.js
@@ -5,6 +5,8 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://bvopen.abrickis.me/#/standings');
+    // to wait for selector untill it properly load by taking it time you can set timeout to 0.
+    //E.g: await page.waitForSelector('.category', { timeout: 0 });
     await page.waitForSelector('.category', { timeout: 1000 });
 
     const body = await page.evaluate(() => {
